44c27560d2a0b90a99f3183b3bb1ca3ba2ccae9f
nantha42 <krnantha13@gmail.com>
2020-06-05 13:54:11 +0530

    New contest

--- CodeForces/boardmoves.cpp
+++ CodeForces/boardmoves.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 #define ll long long
 ll get(ll s){
-  int x=0,y=10;
+  ll x=0,y=10;
   while(s>0){
-    int t = s%10;
+    ll t = s%10;
     s = s/10;
     x = max(x,t);
     y = min(y,t);
@@ -12,22 +12,19 @@ ll get(ll s){
   return x*y;
 }
 int main(){
-  freopen("input.txt","r",stdin);
+  // freopen("input.txt","r",stdin);
   int t;
   cin>>t;
   while(t--){
-      int n,k;
+      ll n,k;
       cin>>n>>k;
       ll s = n;
-      for(int j=1;j<k;j++){
+      for(ll j=1;j<k;j++){
         ll u = get(s);
         if(u==0)
           break;
         s = s+ u;
       }
       cout<<s<<endl;
-
-  }
-  
+  }  
 }
-
