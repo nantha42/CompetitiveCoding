9d5f53c5dac7368e15343c5df3cb74660d8e2c4e
nantha42 <krnantha13@gmail.com>
2020-06-08 09:30:32 +0530

    Troublesort.cpp

--- CodeForces/troublesort.cpp
+++ CodeForces/troublesort.cpp
@@ -2,26 +2,46 @@
 #define ll long long
 using namespace std;
 
-/*bool space(vector<vector<int> > arr){
+void swap(vector<vector<int> > &arr,int j){
+	int k = arr[0][j];
+	arr[0][j] = arr[0][j+1];
+	arr[0][j+1] = k;
+	k = arr[1][j];
+	arr[1][j] = arr[1][j+1];
+	arr[1][j+1] = k;
+}
+
+bool space(vector<vector<int> > arr){
+	
+
 	int s = arr[0].size();
 	for(int i=0;i<s*2;i++){
 		for(int j=0;j<s-1;j++){
-			if(arr[1][j]!=arr[1][j+1]){
-				if(arr[0][j]>arr[0][j+1]){
-					cout<<arr[0][j]<<" "<<arr[0][j+1]<<endl;
-					int k = arr[0][j];
-					arr[0][j] = arr[0][j+1];
-					arr[0][j+1] = k;
-					k = arr[1][j];
-					arr[1][j] = arr[1][j+1];
-					arr[1][j+1] = k;
+			
+			if(arr[0][j]>arr[0][j+1]){
+				if(arr[1][j]!=arr[1][j+1]){
+					swap(arr,j);
+				}
+				else{
+					bool can = false;
+					for(int k=0;k<s;k++){
+						if(k!=j && k!=j+1){
+							if(arr[1][k]!=arr[1][j]){
+								swap(arr,j);
+								can = true;
+								break;
+							}
+						}
+					}
+					if(!can)
+						return false;
 				}
 			}
 		}
 	}
-	for(int i=0;i<s;i++)
-		cout<<arr[0][i]<<" ";
-	cout<<endl;
+	// for(int i=0;i<s;i++)
+	// 	cout<<arr[0][i]<<" ";
+	// cout<<endl;
 	for(int i=0;i<s-1;i++){
 		if(arr[0][i]>arr[0][i+1]){
 			return false;
@@ -29,43 +49,31 @@ using namespace std;
 	}
 	return true;
 }
-*/
 
-bool space(vector<vector<int> > arr){
-	int o=0,z=0;
-	for(int i=0;i<arr[1].size();i++){
-		if(arr[1][i]==1)
-			o++;
-		else
-			z++;
-	}
-	if(o==0 || z==0)
-		return false;
-	return true;
-}
+
+
 int main(){
 	freopen("input.txt","r",stdin);
-	
 	int n;
 	cin>>n;
 	while(n--){
 		int a;
 		cin>>a;
 		vector<vector<int>> mat(2);
-		int q;
 		for(int i=0;i<a;i++){
-			cin>>q;
-			mat[0].push_back(q);
+			int c;
+			cin>>c;
+			mat[0].push_back(c);
 		}
 		for(int i=0;i<a;i++){
-			cin>>q;
-			mat[1].push_back(q);
+			int c;
+			cin>>c;
+			mat[1].push_back(c);
 		}
 		if(space(mat))
 			cout<<"Yes"<<endl;
-		else cout<<"No"<<endl;
+		else
+			cout<<"No"<<endl;
 	}
-
-	
 	
 }
